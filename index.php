f19e8abded033417587060cf123160a269e240b0
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-09 00:45:32 +0800

    debug local bootstrap link for homepage

--- index.php
+++ index.php
@@ -5,14 +5,10 @@
   <title>微軟13屆領袖營 - 首頁</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <!-- jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <!-- Bootstrap -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <!-- My Scripts -->
   <script src="#"></script>
   <!-- CSS Style -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 
@@ -24,7 +20,7 @@
   ?>
 
   <div class="container col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2">
-    
+
     <!-- Banner -->
     <div class="banner">
       <img src="png/banner.png" class="img-responsive"/>
@@ -52,10 +48,10 @@
 
 </div>
 
-  <!-- jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-  <!-- Bootstrap Core JavaScript -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="jquery-3.1.0.min.js"></script>
+<!-- Bootstrap script -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
