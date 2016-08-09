2c6d37982cec718e55853204b553ff4129c90dc1
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-09 11:03:47 +0800

    remove js link

--- rules.php
+++ rules.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
   <!-- My Scripts -->
-  <script src="home.js"></script>
+  <script src="#"></script>
   <!-- Bootstrap Core CSS -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- CSS Style -->
