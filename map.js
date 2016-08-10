ff1b9abcddf95c4a9c79b62dae3b7791dfd1db09
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-10 21:19:37 +0800

    append popover to body

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('[data-toggle="popover"]').popover({html: true}).click(function(e) { 
+	$('[data-toggle="popover"]').popover({container: 'body', html: true}).click(function(e) { 
 		e.preventDefault(); 
 		$(this).focus(); 
 	});
