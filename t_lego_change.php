71144e892c5a8e66609b4b7d43456e474b62e68d
kevinwei30 <kevinwei840223@gmail.com>
2016-08-11 12:05:33 +0800

    Add files via upload

--- t_lego_change.php
+++ t_lego_change.php
@@ -78,7 +78,7 @@
 
 <div id="change_btn" class="w3-container w3-section w3-padding-large">
 <button class="w3-btn w3-padding w3-teal w3-round">確定</button><br><br>
-<button class="w3-btn w3-padding w3-teal w3-round">取消</button>
+<a class="w3-btn w3-padding w3-teal w3-round" href="t_lego_list.php">取消</a>
 </div>
 
 </body>
