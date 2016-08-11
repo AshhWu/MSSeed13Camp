8acbc2c8a245ca27be6a553b8497074ae4ce8efe
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-11 11:17:57 +0800

    Update t_taipeiAdmin.php

--- t_taipeiAdmin.php
+++ t_taipeiAdmin.php
@@ -11,24 +11,6 @@
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-    <!-- CSS Style -->
-        <style type="text/css">
-        body { background-color: #fff; border-top: solid 10px #000;
-            color: #333; font-size: .85em; margin: 20px; padding: 20px;
-            font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
-        }
-        h1, h2, h3 { color: #000; margin-bottom: 0; padding-bottom: 0; }
-        h1 { font-size: 2em; }
-        h2 { font-size: 1.75em; }
-        h3 { font-size: 1.2em; }
-        table { margin-top: 0.75em;}
-        th { font-size: 1.2em; text-align: center; border: none 0px; padding-right: 15px;
-            background-color: #4CAF50;
-            color: white; }
-        td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
-
-        tr:nth-child(even){background-color: #f2f2f2}
-        </style>
     </head>
     <body>
     <?php
