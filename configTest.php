6ae7c8bfa6e8f7f0c6d0b162d357e2c5459ac0cd
WangChen Wu <ra960618@gmail.com>
2016-08-16 13:32:40 +0800

    conf Test

--- configTest.php
+++ configTest.php
@@ -5,50 +5,50 @@
 	
 	$connstr = getenv('SQLAZURECONNSTR_MS_TableConnectionString');
 
-	//function connStrToArray($conn_str){
+	// function connStrToArray($conn_str){
  
-    	// Initialize array.
-    	$conn_array = array();
+ //    	// Initialize array.
+ //    	$conn_array = array();
  
-    	// Split conn string on semicolons. Results in array of "parts".
-    	$parts = explode(";", $conn_str);
+ //    	// Split conn string on semicolons. Results in array of "parts".
+ //    	$parts = explode(";", $conn_str);
  
  
-    	// Loop through array of parts. (Each part is a string.)
-    	foreach($parts as $part){
+ //    	// Loop through array of parts. (Each part is a string.)
+ //    	foreach($parts as $part){
  
-        	// Separate each string on equals sign. Results in array of 2 items.
-        	$temp = explode("=", $part);
+ //        	// Separate each string on equals sign. Results in array of 2 items.
+ //        	$temp = explode("=", $part);
  
-        	// Make items key=>value pairs in returned array.
-        	$conn_array[$temp[0]] = $temp[1];
-    	}
+ //        	// Make items key=>value pairs in returned array.
+ //        	$conn_array[$temp[0]] = $temp[1];
+ //    	}
  	
-    	//return $conn_array;
-	//}
+ //    	return $conn_array;
+	// }
 
-	//function OpenConnection()  
-    //{  
-    	$connArr = conn_array;
-    	echo connArr[Data Source];
-    	echo connArr[Initial Catalog];
-    	echo connArr[User ID];
-        try
-        {
-            $serverName = connArr[Data Source];
-            $connectionOptions = array("Database"=>connArr[Initial Catalog],
-                "Uid"=>connArr[User ID], "PWD"=>connArr[Password]);
-            $conn = sqlsrv_connect($serverName, $connectionOptions);
-            if($conn == false)
-                die(FormatErrors(sqlsrv_errors()));
-        }
-        catch(Exception $e)
-        {  
-            echo("Error!");  
-        }  
-    //}
+	// function OpenConnection()  
+ //    {  
+ //    	$connArr = connStrToArray(connstr);
+ //    	echo connArr[Data Source];
+ //    	echo connArr[Initial Catalog];
+ //    	echo connArr[User ID];
+ //        try  
+ //        {  
+ //            $serverName = connArr[Data Source];  
+ //            $connectionOptions = array("Database"=>connArr[Initial Catalog],  
+ //                "Uid"=>connArr[User ID], "PWD"=>connArr[Password]);  
+ //            $conn = sqlsrv_connect($serverName, $connectionOptions);  
+ //            if($conn == false)  
+ //                die(FormatErrors(sqlsrv_errors()));  
+ //        }  
+ //        catch(Exception $e)  
+ //        {  
+ //            echo("Error!");  
+ //        }  
+ //    }
 
-    //OpenConnection();
+ //    OpenConnection();
 ?>
  </body>
- </html>
+  </html>
