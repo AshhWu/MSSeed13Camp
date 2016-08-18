56a9aec2be61c7087fbdbad8d937b2274b044ca9
andyliu42 <andyliu31240@gmail.com>
2016-08-18 15:40:01 +0800

    Revert "debug"
    This reverts commit a406c6eb5d9c87f7a6c378274979eca827dff4b0.

--- auto_add.php
+++ auto_add.php
@@ -13,13 +13,16 @@
 	* limitations under the License. 
 	*/
 	
+# this file automatically execute by windows every minute
 
+
+$host = "ap-cdbr-azure-east-c.cloudapp.net";
+$user = "b37f8ddf38d21d";
+$pwd = "1e72c81e";
+$db = "stronghold";
+	
 function connect()
 {
-	$host = "ap-cdbr-azure-east-c.cloudapp.net";
-	$user = "b37f8ddf38d21d";
-	$pwd = "1e72c81e";
-	$db = "stronghold";
 	// DB connection info
 	try{
 		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
