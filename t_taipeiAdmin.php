7e1f54167c3e1d1f0050ce295131ad0a288ea370
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-19 13:34:34 +0800

    include nav.js

--- t_taipeiAdmin.php
+++ t_taipeiAdmin.php
@@ -172,5 +172,13 @@
 
         <hr/>
         
+
+        
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- My Scripts -->
+    <script src="js/nav.js"></script>
     </body>
 </html>
