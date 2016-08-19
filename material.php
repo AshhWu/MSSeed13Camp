3779bb0f4cc85e325930ad8fb74be10583ff828b
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-19 18:42:10 +0800

    add transportation

--- material.php
+++ material.php
@@ -166,7 +166,7 @@
           </div>
           <div class="col-md-2 col-xs-3 wrapper special-wrapper">
             <div class="count special-count"><?php if(!empty($items)) { echo $items[0][32]; } ?></div>
-            <img src="png/s_special/miss_air.jpg" class="img-responsive icon-special" alt="空姐">
+            <img src="png/s_special/miss_air.png" class="img-responsive icon-special" alt="空姐">
             <p class="name special-name text-center">空姐</p>
           </div>
           <div class="col-md-2 col-xs-3 wrapper special-wrapper">
@@ -236,6 +236,9 @@
             <img src="png/s_component/gun.png" class="img-responsive icon-components" alt="機艙">
             <p class="name component-name text-center">機艙</p>
           </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0"><p class="hint">※合成請點選零件圖示</p></div>
         </div>
       </div>
 
@@ -607,6 +610,81 @@
 
       <!-- Transportation -->
       <div id="transport" class="tab-pane fade">
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#aladdin_carpet">
+            <p class="name transport-name text-center">阿拉丁的飛毯</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#space_shuttle">
+            <p class="name transport-name text-center">太空梭</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#nimbus_2000">
+            <p class="name transport-name text-center">光輪2000</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#bamboo_dragonfly">
+            <p class="name transport-name text-center">竹蜻蜓</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#santa_sled">
+            <p class="name transport-name text-center">聖誕雪橇</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#moving_castle">
+            <p class="name transport-name text-center">霍爾的移動城堡</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#superman_cape">
+            <p class="name transport-name text-center">超人披風</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#somersault_cloud">
+            <p class="name transport-name text-center">筋斗雲</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#batmobile">
+            <p class="name transport-name text-center">蝙蝠車</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#balloon">
+            <p class="name transport-name text-center">氣球</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#hang_gliding">
+            <p class="name transport-name text-center">滑翔翼</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#hot_air_balloon">
+            <p class="name transport-name text-center">熱氣球</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#baymax">
+            <p class="name transport-name text-center">杯麵</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#helicopter">
+            <p class="name transport-name text-center">直升機</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#fighter_plane">
+            <p class="name transport-name text-center">戰鬥機</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#airship">
+            <p class="name transport-name text-center">飛艇</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#airliner">
+            <p class="name transport-name text-center">客機</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#ufo">
+            <p class="name transport-name text-center">飛碟</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#hermes_shoes">
+            <p class="name transport-name text-center">荷米斯的鞋子</p>
+          </div>
+          <div class="col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#paragliding">
+            <p class="name transport-name text-center">飛行傘</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#wright_flyer">
+            <p class="name transport-name text-center">飛行者一號</p>
+          </div>
+        </div>
       </div>
 
     </div>
