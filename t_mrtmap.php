f2de16ab601bf8cb6456dfc798bfad880647b5f4
kevinwei30 <kevinwei840223@gmail.com>
2016-08-20 12:05:06 +0800

    Add files via upload

--- t_mrtmap.php
+++ t_mrtmap.php
@@ -40,7 +40,7 @@
 <div class="middle_part w3-container w3-section">
 	<h5 class="w3-brown w3-padding w3-round-large">樂高</h5>
 	<section class="section2 w3-container">
-		<?php $team=1; ?>
+		<?php $team=20; ?>
 		<div id="lego2" class="w3-tag w3-red"><p><?php echo getCubeNum($team, 2)?></p></div>
 		<div id="lego3" class="w3-tag w3-green"><p><?php echo getCubeNum($team, 3)?></p></div>
 		<div id="lego4" class="w3-tag w3-yellow"><p><?php echo getCubeNum($team, 4)?></p></div>
