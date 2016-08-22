a5bb592390290eadc8f76374c54b942e6621162e
kevinwei30 <kevinwei840223@gmail.com>
2016-08-23 04:46:47 +0800

    Add files via upload

--- t_lego_list.php
+++ t_lego_list.php
@@ -33,7 +33,7 @@
 
 	<div class="tab-content">
 		<div id="lego_list" class="tab-pane fade in active w3-container w3-section w3-padding">
-			<h4 class="w3-card-4 w3-pink w3-padding">各組樂高(點組別按鈕請求交易)</h4>
+			<h4 class="w3-card-4 w3-pink w3-padding">點組別按鈕請求交易</h4>
 			<ul class="w3-ul">
 				<li class="w3-container">
 					<?php $team=2; ?>
