08cf163f47ead83b4fef72ab7751b0624cd89c90
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-23 14:27:04 +0800

    unset all session variable when logout

--- backend/login/logout.php
+++ backend/login/logout.php
@@ -3,6 +3,8 @@
 <?php
 //將session清空
 unset($_SESSION['ID']);
+unset($_SESSION['authority']);
+session_unset();
 echo '登出中......';
 echo '<meta http-equiv=REFRESH CONTENT=1;url=../../index.php>';
 ?>
