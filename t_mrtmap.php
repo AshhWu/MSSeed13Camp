e34154e679b4f94a402d656b86420e3f57249ecb
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-23 14:40:07 +0800

    add ;

--- t_mrtmap.php
+++ t_mrtmap.php
@@ -42,10 +42,10 @@ session_start();
 <div class="middle_part w3-container w3-section">
 	<h5 class="w3-brown w3-padding">樂高</h5>
 	<section class="section2 w3-container">
-		<div id="lego2" class="w3-tag w3-red"><p><?php echo $lego[1]?></p></div>
-		<div id="lego3" class="w3-tag w3-green"><p><?php echo $lego[2]?></p></div>
-		<div id="lego4" class="w3-tag w3-yellow"><p><?php echo $lego[3]?></p></div>
-		<div id="lego5" class="w3-tag w3-blue"><p><?php echo $lego[4]?></p></div>
+		<div id="lego2" class="w3-tag w3-red"><p><?php echo $lego[1]; ?></p></div>
+		<div id="lego3" class="w3-tag w3-green"><p><?php echo $lego[2]; ?></p></div>
+		<div id="lego4" class="w3-tag w3-yellow"><p><?php echo $lego[3]; ?></p></div>
+		<div id="lego5" class="w3-tag w3-blue"><p><?php echo $lego[4]; ?></p></div>
 	</section>
 </div>
 
