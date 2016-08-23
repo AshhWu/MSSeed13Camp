44d35d2ea95da7e314b380c2e2dfc4a40d0cc4fd
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-24 02:38:15 +0800

    use logout button to logout

--- js/nav.js
+++ js/nav.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('[data-toggle=dropdown]').each(function() {
  		this.addEventListener('click', function() {}, false);
 	});
+
+	$('.logout-btn').click(function(){
+		$.post("/backend/login/logout.php");
+	});
 });
--- material.php
+++ material.php
@@ -2,7 +2,6 @@
 session_start();
 header('Cache-Control: no-cache');
 header('Pragma: no-cache');
-$_SESSION["s_team"] = 'B'; //testing
 ?>
 
 <!DOCTYPE html>
