4e9bdc916ba6177ab41e276776fc3728147712db
kevinwei30 <kevinwei840223@gmail.com>
2016-08-24 00:05:02 +0800

    Add files via upload

--- js/t_main.js
+++ js/t_main.js
@@ -194,12 +194,11 @@ function trade_init(trade){
 }
 
 function wait_init(wait){
-	alert(wait);
 	var i, sender;
 	var tmp_str;
-	for (i = 0; i < wait.length; i++){
+	for (i = 0; i < wait.length-1; i++){
 		sender = wait[i][2];
-		tmp_str = '<li class="w3-container"><p>你向第' + sneder.toString() + '組提出的交易</p></li>';
+		tmp_str = '<li class="w3-container"><p>你向第' + sender.toString() + '組提出的交易</p></li>';
 		document.getElementById("wait_ul").innerHTML += tmp_str;
 	}
 }
