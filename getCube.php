b08316f54cca4379475459f127d773075b532ce1
WangChen Wu <ra960618@gmail.com>
2016-08-24 23:48:41 +0800

    test database

--- getCube.php
+++ getCube.php
@@ -2,9 +2,9 @@
 include_once 'backend/taskmodel.php';
 echo "start!!";
 
-$conn = connect();
-$sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
-$result = $conn->query($query) or die($conn->error.__LINE__);
+// $conn = connect();
+// $sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
+// $result = $conn->query($query) or die($conn->error.__LINE__);
 
 $sql = "SELECT cube1, cube2, cube3, cube4, cube5 FROM taipeiRun where team=".$team;
 	$stmt = $conn->query($sql);
