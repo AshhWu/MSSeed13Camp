8c724f054522cc15a776be8e7b61583f413b67ba
WangChen Wu <ra960618@gmail.com>
2016-08-24 22:47:27 +0800

    Test new database

--- backend/connTest.php
+++ backend/connTest.php
@@ -1,37 +1,33 @@
 <?php
     
-        $connectstr_dbhost = '';
-        $connectstr_dbname = 'msseed13';
-        $connectstr_dbusername = '';
-        $connectstr_dbpassword = '';
-        
-        foreach ($_SERVER as $key => $value) {
-            if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-                continue;
-            }
-            
-            $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-            $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-            $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-        }
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
     
-        $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$ connectstr_dbname)   ;
-        
-        if (!$conn) {
-            echo "Error: Unable to connect to MySQL." . PHP_EOL;
-            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-            exit;
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
         }
         
-        echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-        echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
-    
-    
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
 
-    // $sql = "SELECT * FROM account";
-    // $stmt = $conn->query($sql);
-    // echo $stmt->fetchAll(PDO::FETCH_NUM);
+    $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)   ;
     
+    if (!$conn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+    
+    mysqli_close($link);
 
 ?>
