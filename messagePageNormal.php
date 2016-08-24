7bf12ecf59a98a8690ab6aa754669087184517a6
andyliu42 <andyliu31240@gmail.com>
2016-08-24 17:03:07 +0800

    Debug

--- messagePageNormal.php
+++ messagePageNormal.php
@@ -1,12 +1,8 @@
-<?php 
-session_start();
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
 <!-- Title -->
-<title>消息公告</title>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
@@ -33,10 +29,6 @@ session_start();
 </head>
 
 <body>
-<?php
-  include "backend/navbar/nav.php";
-if($_SESSION['ID'] != null){ 
-  ?>
   
 <div style="text-align:center" align="center">
 	<h1>Messages</h1>
@@ -63,25 +55,6 @@ if($_SESSION['ID'] != null){
 ?>
 
 </div>
-
-<?php
-}
-else{
-	echo "<script> alert('您無權觀看此頁面喔')</script>";
-	echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
-}
-?>
-<!--
-<p>
-	<span style="font-size:14px;">11:50</span></p>
-<h3 style="color:red;">
-	【警告】10分鐘過後噴火龍將會襲擊大家的陣營，請盡速回首並做好防護措施</h3>
-<hr />
-<p>
-	<span style="font-size:14px;">11:32</span></p>
-<h3 style="color:blue;">
-	礦產大放送：現在只要收集到五個木材就可以到礦場換一個鑽石！</h3>
--->
 
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
