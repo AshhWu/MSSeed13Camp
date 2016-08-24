70b6a0defeb509fb3bcd4c6b65a81760ad09faa2
WangChen Wu <ra960618@gmail.com>
2016-08-25 00:20:02 +0800

    db test

---
+++ GMCentral/Taipei/CURD/db.php
@@ -0,0 +1,30 @@
+<?php
+echo "start!!";
+	$host = "ap-cdbr-azure-east-c.cloudapp.net";
+	$user = "b37f8ddf38d21d";
+	$pwd = "1e72c81e";
+	$db = "stronghold";
+
+	try{
+		$conn = new mysqli($host, $user, $pwd, $db);
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	echo "success1!";
+$sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
+$result = $conn->query($query) or die($conn->error.__LINE__);
+
+echo "success2!";
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
+
+# JSON-encode the response
+echo "success3!";
+echo $json_response = json_encode($arr);
+?>
