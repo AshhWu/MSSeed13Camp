1dd79ad8384971d78f5f0517247277b46f2e35c4
WangChen Wu <ra960618@gmail.com>
2016-08-24 23:47:20 +0800

    test database

--- getCube.php
+++ getCube.php
@@ -1,19 +1,25 @@
 <?php
 include_once 'backend/taskmodel.php';
-echo "start!";
+echo "start!!";
 
 $conn = connect();
 $sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
 $result = $conn->query($query) or die($conn->error.__LINE__);
 
-$arr = array();
-if($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$arr[] = $row;	
-	}
-}
+$sql = "SELECT cube1, cube2, cube3, cube4, cube5 FROM taipeiRun where team=".$team;
+	$stmt = $conn->query($sql);
+    $items = $stmt->fetchAll(PDO::FETCH_NUM);
+echo "success!";
+echo $items;
+
+// $arr = array();
+// if($result->num_rows > 0) {
+// 	while($row = $result->fetch_assoc()) {
+// 		$arr[] = $row;	
+// 	}
+// }
 
 # JSON-encode the response
 echo "success!";
-echo $json_response = json_encode($arr);
+//echo $json_response = json_encode($arr);
 ?>
