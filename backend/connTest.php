3767eb47f5b9552747f3e78742053920b40bf339
WangChen Wu <ra960618@gmail.com>
2016-08-24 22:45:32 +0800

    Test new database

--- backend/connTest.php
+++ backend/connTest.php
@@ -17,7 +17,7 @@
     
         $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$ connectstr_dbname)   ;
         
-        if (!$link) {
+        if (!$conn) {
             echo "Error: Unable to connect to MySQL." . PHP_EOL;
             echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
             echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
@@ -25,7 +25,7 @@
         }
         
         echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-        echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+        echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
     
     
 
