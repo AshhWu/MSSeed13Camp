ae0feb1a5ed5a82ca846eb3e122b09a56da27573
WangChen Wu <ra960618@gmail.com>
2016-08-24 23:32:37 +0800

    add getLocation.php

---
+++ GMCentral/Taipei/CURD/getLocation.php
@@ -0,0 +1,17 @@
+<?php
+include_once '../../../backend/taskmodel.php';
+
+$conn = connect();
+$sql = "SELECT team, gpsX, gpsY FROM taipeiRun";
+$result = $conn->query($query) or die($conn->error.__LINE__);
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
+
+# JSON-encode the response
+echo $json_response = json_encode($arr);
+?>
