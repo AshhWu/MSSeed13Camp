e1278f683fc31ae8910cb90c89523e10935931d1
andyliu42 <andyliu31240@gmail.com>
2016-08-24 17:14:08 +0800

    Debug

--- messagePage.php
+++ messagePage.php
@@ -38,8 +38,8 @@ session_start();
 if($_SESSION['ID'] != null){ 
   ?>
  
-    <iframe src="messagePageGM.php" width="100%" height="100">
-    <iframe src="messagePageNormal.php" width="100%" height="200">
+    <iframe src="messagePageGM.php" width="100%" height="180"></iframe>
+    <iframe src="messagePageNormal.php" width="100%" height="220"></iframe>
 		
 <?php
 }
