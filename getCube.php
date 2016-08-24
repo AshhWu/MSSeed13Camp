b29d55af0a6470dc2c2505d40d559fa6789bb1cf
WangChen Wu <ra960618@gmail.com>
2016-08-25 00:10:29 +0800

    test db

--- getCube.php
+++ getCube.php
@@ -2,26 +2,20 @@
 include_once 'backend/taskmodel.php';
 echo "start!!";
 
-// $conn = connect();
-// $sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
-// $result = $conn->query($query) or die($conn->error.__LINE__);
+$conn = connect();
+$sql = "SELECT team, cube2, cube3, cube4, cube5, mission FROM taipeiRun";
+$result = $conn->query($query) or die($conn->error.__LINE__);
 
-    $conn = connect();
-    $sql = "SELECT team, cube1, cube2, cube3, cube4, cube5 FROM taipeiRun ORDER BY team ASC";
-	$stmt = $conn->query($sql);
-    $items = $stmt->fetchAll(PDO::FETCH_NUM);
-    
 echo "success!";
-echo $items;
 
-// $arr = array();
-// if($result->num_rows > 0) {
-// 	while($row = $result->fetch_assoc()) {
-// 		$arr[] = $row;	
-// 	}
-// }
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
 
 # JSON-encode the response
 echo "success!";
-//echo $json_response = json_encode($arr);
+echo $json_response = json_encode($arr);
 ?>
