37ebf957f980c0f6f0de5d30872fdbee23f487b1
houtoto13579 <houtoto13579@gmail.com>
2016-08-24 13:39:47 +0800

    add php debug
    QQ why~~

--- GMCentral/assets/js/slider.js
+++ GMCentral/assets/js/slider.js
@@ -337,7 +337,8 @@ setNewGraph = function(obj){
 	var obj = JSON.parse("[[\"A\",\"500\"],[\"B\",\"300\"]]");
 	//Debug//
 	//console.log(obj[0][1]);
-	x2[0] = x2[0]+100;
+	//x2[0] = x2[0]+100;
+	x2[0] = x2[0]+obj[0][1];
 	console.log(x2[0]);
 }
 /* Down Below is Function to Set Graph */
