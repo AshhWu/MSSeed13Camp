0d7d55dd16ec31e9c82ff249870f0aa47a09d6fe
houtoto13579 <houtoto13579@gmail.com>
2016-08-24 11:18:04 +0800

    Add resources php parsing in GMCentral
    Finish in animation change
    Hope it will work >_0

--- GMCentral/assets/js/slider.js
+++ GMCentral/assets/js/slider.js
@@ -2,11 +2,317 @@
 renew = function(){
 	var resourcesObj = getAllResources();
 	setNewGraph(resourcesObj);
-	$("#Seconddiv").load(location.href + " #Seconddiv",function () {
+	$("#main").load(location.href + " #main",function () {
          $(this).unwrap();
-    });	
+    });
+	chartMaker();
 }
+chartMaker = function(){
+		var chart = AmCharts.makeChart("chartdiv3", {
+                "theme": "black",
+                "type": "serial",
+                "startDuration": 1,
+                "dataProvider": [{
+                    "country": "A",
+                    "visits": x2[0],
+                    "color": "#FF0F00"
+                }, {
+                    "country": "B",
+                    "visits": x2[1],
+                    "color": "#FF6600"
+                }, {
+                    "country": "C",
+                    "visits": x2[2],
+                    "color": "#FF9E01"
+                }, {
+                    "country": "D",
+                    "visits": x2[3],
+                    "color": "#FCD202"
 
+                }],
+                "valueAxes": [{
+                    "position": "left",
+                    "title": "Visitors"
+                }],
+                "graphs": [{
+                    "balloonText": "[[category]]: <b>[[value]]</b>",
+                    "fillColorsField": "color",
+                    "fillAlphas": 1,
+                    "lineAlpha": 0.1,
+                    "type": "column",
+                    "valueField": "visits"
+                }],
+                "depth3D": 20,
+                "angle": 30,
+                "chartCursor": {
+                    "categoryBalloonEnabled": false,
+                    "cursorAlpha": 0,
+                    "zoomable": false
+                },
+                "categoryField": "country",
+                "categoryAxis": {
+                    "gridPosition": "start",
+                    "labelRotation": 90
+                },
+                "export": {
+                    "enabled": false
+                }
+
+		    });
+   		var chart = AmCharts.makeChart("chartdiv4", {
+                "theme": "black",
+                "type": "serial",
+                "startDuration": 0,
+                "dataProvider": [{
+                    "country": "A",
+                    "visits": 4025,
+                    "color": "#FF0F00"
+                }, {
+                    "country": "B",
+                    "visits": 1882,
+                    "color": "#FF6600"
+                }, {
+                    "country": "C",
+                    "visits": 1809,
+                    "color": "#FF9E01"
+                }, {
+                    "country": "D",
+                    "visits": 1322,
+                    "color": "#FCD202"
+                
+                }],
+                "valueAxes": [{
+                    "position": "left",
+                    "axisAlpha":0,
+                    "gridAlpha":0
+                }],
+                "graphs": [{
+                    "balloonText": "[[category]]: <b>[[value]]</b>",
+                    "colorField": "color",
+                    "fillAlphas": 0.85,
+                    "lineAlpha": 0.1,
+                    "type": "column",
+                    "topRadius":1,
+                    "valueField": "visits"
+                }],
+                "depth3D": 40,
+                "angle": 30,
+                "chartCursor": {
+                    "categoryBalloonEnabled": false,
+                    "cursorAlpha": 0,
+                    "zoomable": false
+                },
+                "categoryField": "country",
+                "categoryAxis": {
+                    "gridPosition": "start",
+                    "axisAlpha":0,
+                    "gridAlpha":0
+                },
+                "export": {
+                    "enabled": false
+                }}, 0);
+		var chart = AmCharts.makeChart("chartdiv2", {
+			"theme": "black",
+			"type": "serial",
+			"startDuration": 0,
+			"dataProvider": [{
+				"country": "A",
+				"visits": 40205,
+				"color": "#FF0F00"
+			}, {
+				"country": "B",
+				"visits": 1882,
+				"color": "#FF6600"
+			}, {
+				"country": "C",
+				"visits": 1809,
+				"color": "#FF9E01"
+			}, {
+				"country": "D",
+				"visits": 1322,
+				"color": "#FCD202"
+			
+			}],
+			"graphs": [{
+				"balloonText": "[[category]]: <b>[[value]]</b>",
+				"fillColorsField": "color",
+				"fillAlphas": 1,
+				"lineAlpha": 0.1,
+				"type": "column",
+				"valueField": "visits"
+			}],
+			"depth3D": 20,
+			"angle": 30,
+			"chartCursor": {
+				"categoryBalloonEnabled": false,
+				"cursorAlpha": 0,
+				"zoomable": false
+			},
+			"categoryField": "country",
+			"categoryAxis": {
+				"gridPosition": "start",
+				"labelRotation": 90
+			},
+			"export": {
+				"enabled": false
+			 }
+
+		});
+
+        var chart = AmCharts.makeChart("chartdiv1", {
+			"theme": "black",
+			"type": "serial",
+			"startDuration": 0,
+			"dataProvider": [{
+				"country": "A",
+				"visits": 4025,
+				"color": "#FF0F00"
+			}, {
+				"country": "B",
+				"visits": 1882,
+				"color": "#FF6600"
+			}, {
+				"country": "C",
+				"visits": 1809,
+				"color": "#FF9E01"
+			}, {
+				"country": "D",
+				"visits": 1322,
+				"color": "#FCD202"
+			
+			}],
+			"valueAxes": [{
+				"position": "left",
+				"axisAlpha":0,
+				"gridAlpha":0
+			}],
+			"graphs": [{
+				"balloonText": "[[category]]: <b>[[value]]</b>",
+				"colorField": "color",
+				"fillAlphas": 0.85,
+				"lineAlpha": 0.1,
+				"type": "column",
+				"topRadius":1,
+				"valueField": "visits"
+			}],
+			"depth3D": 40,
+			"angle": 30,
+			"chartCursor": {
+				"categoryBalloonEnabled": false,
+				"cursorAlpha": 0,
+				"zoomable": false
+			},
+			"categoryField": "country",
+			"categoryAxis": {
+				"gridPosition": "start",
+				"axisAlpha":0,
+				"gridAlpha":0
+			},
+			"export": {
+				"enabled": false
+			}}, 0);
+		var chart = AmCharts.makeChart("chartdiv01", {
+			"theme": "black",
+			"type": "serial",
+			"startDuration": 0,
+			"dataProvider": [{
+				"country": "A",
+				"visits": 40205,
+				"color": "#FF0F00"
+			}, {
+				"country": "B",
+				"visits": 20182,
+				"color": "#FF6600"
+			}, {
+				"country": "C",
+				"visits": 20009,
+				"color": "#FF9E01"
+			}, {
+				"country": "D",
+				"visits": 18022,
+				"color": "#FCD202"
+			
+			}],
+			"graphs": [{
+				"balloonText": "[[category]]: <b>[[value]]</b>",
+				"fillColorsField": "color",
+				"fillAlphas": 1,
+				"lineAlpha": 0.1,
+				"type": "column",
+				"valueField": "visits"
+			}],
+			"depth3D": 20,
+			"angle": 30,
+			"chartCursor": {
+				"categoryBalloonEnabled": false,
+				"cursorAlpha": 0,
+				"zoomable": false
+			},
+			"categoryField": "country",
+			"categoryAxis": {
+				"gridPosition": "start",
+				"labelRotation": 90
+			},
+			"export": {
+				"enabled": false
+			 }
+
+		});
+
+        var chart = AmCharts.makeChart("chartdiv02", {
+			"theme": "black",
+			"type": "serial",
+			"startDuration": 0,
+			"dataProvider": [{
+				"country": "A",
+				"visits": 4025,
+				"color": "#FF0F00"
+			}, {
+				"country": "B",
+				"visits": 1882,
+				"color": "#FF6600"
+			}, {
+				"country": "C",
+				"visits": 1809,
+				"color": "#FF9E01"
+			}, {
+				"country": "D",
+				"visits": 1322,
+				"color": "#FCD202"
+			
+			}],
+			"valueAxes": [{
+				"position": "left",
+				"axisAlpha":0,
+				"gridAlpha":0
+			}],
+			"graphs": [{
+				"balloonText": "[[category]]: <b>[[value]]</b>",
+				"colorField": "color",
+				"fillAlphas": 0.85,
+				"lineAlpha": 0.1,
+				"type": "column",
+				"topRadius":1,
+				"valueField": "visits"
+			}],
+			"depth3D": 40,
+			"angle": 30,
+			"chartCursor": {
+				"categoryBalloonEnabled": false,
+				"cursorAlpha": 0,
+				"zoomable": false
+			},
+			"categoryField": "country",
+			"categoryAxis": {
+				"gridPosition": "start",
+				"axisAlpha":0,
+				"gridAlpha":0
+			},
+			"export": {
+				"enabled": false
+			}}, 0);
+
+}
 ///  Ajax TODO  ///
 
 function getAllResources() {
@@ -26,7 +332,8 @@ function getAllResources() {
 var x2 = [10,10,10,10];
 /* Set json in each graph */
 setNewGraph = function(obj){
-	x2[0] = x2[0]+5;
+	x2[0] = obj[0][0];
+	console.log(x2[0]);
 }
 /* Down Below is Function to Set Graph */
 jssor_2_slider_init = function() {
@@ -67,109 +374,7 @@ jssor_2_slider_init = function() {
             $Jssor$.$AddEvent(window, "resize", ScaleSlider);
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
 
-            var chart = AmCharts.makeChart("chartdiv3", {
-                "theme": "black",
-                "type": "serial",
-                "startDuration": 0,
-                "dataProvider": [{
-                    "country": "1",
-                    "visits": x2[0],
-                    "color": "#FF0F00"
-                }, {
-                    "country": "2",
-                    "visits": x2[1],
-                    "color": "#FF6600"
-                }, {
-                    "country": "3",
-                    "visits": x2[2],
-                    "color": "#FF9E01"
-                }, {
-                    "country": "4",
-                    "visits": x2[3],
-                    "color": "#FCD202"
-
-                }],
-                "valueAxes": [{
-                    "position": "left",
-                    "title": "Visitors"
-                }],
-                "graphs": [{
-                    "balloonText": "[[category]]: <b>[[value]]</b>",
-                    "fillColorsField": "color",
-                    "fillAlphas": 1,
-                    "lineAlpha": 0.1,
-                    "type": "column",
-                    "valueField": "visits"
-                }],
-                "depth3D": 20,
-                "angle": 30,
-                "chartCursor": {
-                    "categoryBalloonEnabled": false,
-                    "cursorAlpha": 0,
-                    "zoomable": false
-                },
-                "categoryField": "country",
-                "categoryAxis": {
-                    "gridPosition": "start",
-                    "labelRotation": 90
-                },
-                "export": {
-                    "enabled": false
-                }
-
-		    });
-            var chart = AmCharts.makeChart("chartdiv4", {
-                "theme": "black",
-                "type": "serial",
-                "startDuration": 0,
-                "dataProvider": [{
-                    "country": "1",
-                    "visits": 4025,
-                    "color": "#FF0F00"
-                }, {
-                    "country": "2",
-                    "visits": 1882,
-                    "color": "#FF6600"
-                }, {
-                    "country": "3",
-                    "visits": 1809,
-                    "color": "#FF9E01"
-                }, {
-                    "country": "4",
-                    "visits": 1322,
-                    "color": "#FCD202"
-                
-                }],
-                "valueAxes": [{
-                    "position": "left",
-                    "axisAlpha":0,
-                    "gridAlpha":0
-                }],
-                "graphs": [{
-                    "balloonText": "[[category]]: <b>[[value]]</b>",
-                    "colorField": "color",
-                    "fillAlphas": 0.85,
-                    "lineAlpha": 0.1,
-                    "type": "column",
-                    "topRadius":1,
-                    "valueField": "visits"
-                }],
-                "depth3D": 40,
-                "angle": 30,
-                "chartCursor": {
-                    "categoryBalloonEnabled": false,
-                    "cursorAlpha": 0,
-                    "zoomable": false
-                },
-                "categoryField": "country",
-                "categoryAxis": {
-                    "gridPosition": "start",
-                    "axisAlpha":0,
-                    "gridAlpha":0
-                },
-                "export": {
-                    "enabled": false
-                }}, 0);
+            
         }
 
 
@@ -210,106 +415,7 @@ jssor_1_slider_init = function() {
             $Jssor$.$AddEvent(window, "resize", ScaleSlider);
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
             //responsive code end
-            var chart = AmCharts.makeChart("chartdiv2", {
-			"theme": "black",
-			"type": "serial",
-			"startDuration": 0,
-			"dataProvider": [{
-				"country": "1",
-				"visits": 40205,
-				"color": "#FF0F00"
-			}, {
-				"country": "2",
-				"visits": 1882,
-				"color": "#FF6600"
-			}, {
-				"country": "3",
-				"visits": 1809,
-				"color": "#FF9E01"
-			}, {
-				"country": "4",
-				"visits": 1322,
-				"color": "#FCD202"
-			
-			}],
-			"graphs": [{
-				"balloonText": "[[category]]: <b>[[value]]</b>",
-				"fillColorsField": "color",
-				"fillAlphas": 1,
-				"lineAlpha": 0.1,
-				"type": "column",
-				"valueField": "visits"
-			}],
-			"depth3D": 20,
-			"angle": 30,
-			"chartCursor": {
-				"categoryBalloonEnabled": false,
-				"cursorAlpha": 0,
-				"zoomable": false
-			},
-			"categoryField": "country",
-			"categoryAxis": {
-				"gridPosition": "start",
-				"labelRotation": 90
-			},
-			"export": {
-				"enabled": false
-			 }
-
-		});
-
-        var chart = AmCharts.makeChart("chartdiv1", {
-			"theme": "black",
-			"type": "serial",
-			"startDuration": 0,
-			"dataProvider": [{
-				"country": "1",
-				"visits": 4025,
-				"color": "#FF0F00"
-			}, {
-				"country": "2",
-				"visits": 1882,
-				"color": "#FF6600"
-			}, {
-				"country": "3",
-				"visits": 1809,
-				"color": "#FF9E01"
-			}, {
-				"country": "4",
-				"visits": 1322,
-				"color": "#FCD202"
-			
-			}],
-			"valueAxes": [{
-				"position": "left",
-				"axisAlpha":0,
-				"gridAlpha":0
-			}],
-			"graphs": [{
-				"balloonText": "[[category]]: <b>[[value]]</b>",
-				"colorField": "color",
-				"fillAlphas": 0.85,
-				"lineAlpha": 0.1,
-				"type": "column",
-				"topRadius":1,
-				"valueField": "visits"
-			}],
-			"depth3D": 40,
-			"angle": 30,
-			"chartCursor": {
-				"categoryBalloonEnabled": false,
-				"cursorAlpha": 0,
-				"zoomable": false
-			},
-			"categoryField": "country",
-			"categoryAxis": {
-				"gridPosition": "start",
-				"axisAlpha":0,
-				"gridAlpha":0
-			},
-			"export": {
-				"enabled": false
-			}}, 0);
+            
         };
 jssor_0_slider_init = function() {   
     
@@ -348,104 +454,5 @@ jssor_0_slider_init = function() {
             $Jssor$.$AddEvent(window, "resize", ScaleSlider);
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
             //responsive code end
-            var chart = AmCharts.makeChart("chartdiv01", {
-			"theme": "black",
-			"type": "serial",
-			"startDuration": 0,
-			"dataProvider": [{
-				"country": "1",
-				"visits": 40205,
-				"color": "#FF0F00"
-			}, {
-				"country": "2",
-				"visits": 20182,
-				"color": "#FF6600"
-			}, {
-				"country": "3",
-				"visits": 20009,
-				"color": "#FF9E01"
-			}, {
-				"country": "4",
-				"visits": 18022,
-				"color": "#FCD202"
-			
-			}],
-			"graphs": [{
-				"balloonText": "[[category]]: <b>[[value]]</b>",
-				"fillColorsField": "color",
-				"fillAlphas": 1,
-				"lineAlpha": 0.1,
-				"type": "column",
-				"valueField": "visits"
-			}],
-			"depth3D": 20,
-			"angle": 30,
-			"chartCursor": {
-				"categoryBalloonEnabled": false,
-				"cursorAlpha": 0,
-				"zoomable": false
-			},
-			"categoryField": "country",
-			"categoryAxis": {
-				"gridPosition": "start",
-				"labelRotation": 90
-			},
-			"export": {
-				"enabled": false
-			 }
-
-		});
-
-        var chart = AmCharts.makeChart("chartdiv02", {
-			"theme": "black",
-			"type": "serial",
-			"startDuration": 0,
-			"dataProvider": [{
-				"country": "1",
-				"visits": 4025,
-				"color": "#FF0F00"
-			}, {
-				"country": "2",
-				"visits": 1882,
-				"color": "#FF6600"
-			}, {
-				"country": "3",
-				"visits": 1809,
-				"color": "#FF9E01"
-			}, {
-				"country": "4",
-				"visits": 1322,
-				"color": "#FCD202"
-			
-			}],
-			"valueAxes": [{
-				"position": "left",
-				"axisAlpha":0,
-				"gridAlpha":0
-			}],
-			"graphs": [{
-				"balloonText": "[[category]]: <b>[[value]]</b>",
-				"colorField": "color",
-				"fillAlphas": 0.85,
-				"lineAlpha": 0.1,
-				"type": "column",
-				"topRadius":1,
-				"valueField": "visits"
-			}],
-			"depth3D": 40,
-			"angle": 30,
-			"chartCursor": {
-				"categoryBalloonEnabled": false,
-				"cursorAlpha": 0,
-				"zoomable": false
-			},
-			"categoryField": "country",
-			"categoryAxis": {
-				"gridPosition": "start",
-				"axisAlpha":0,
-				"gridAlpha":0
-			},
-			"export": {
-				"enabled": false
-			}}, 0);
+            
         };
