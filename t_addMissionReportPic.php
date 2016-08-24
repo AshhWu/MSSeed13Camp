8c02a3bae42d94adf3b49d0197742e2aa06cd5da
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-24 22:05:40 +0800

    Add files via upload

---
+++ t_addMissionReportPic.php
@@ -0,0 +1,16 @@
+<?php
+if ($_FILES["file"]["error"] > 0){
+　echo "Error: " . $_FILES["file"]["error"];
+}else{
+　echo "檔案名稱: " . $_FILES["file"]["name"]."<br/>";
+　echo "檔案類型: " . $_FILES["file"]["type"]."<br/>";
+　echo "檔案大小: " . ($_FILES["file"]["size"] / 1024)." Kb<br />";
+　echo "暫存名稱: " . $_FILES["file"]["tmp_name"];
+　
+　if (file_exists("upload/" . $_FILES["file"]["name"])){
+　　echo "檔案已經存在，請勿重覆上傳相同檔案";
+　}else{
+　　move_uploaded_file($_FILES["file"]["tmp_name"],"./TaipeiRun/pic/report/".$_FILES["file"]["name"]);
+　}
+}
+?>
---
+++ t_uploadTest.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form action="t_addMissionReportPic.php" method="post" enctype="multipart/form-data">
+檔案名稱:<input type="file" name="file" id="file" /><br />
+<input type="submit" name="submit" value="上傳檔案" />
+</form>
+
+</body>
+</html>
