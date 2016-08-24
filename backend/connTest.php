523e6dbd06ed556da0aed110f48e8749c14b7c08
WangChen Wu <ra960618@gmail.com>
2016-08-24 22:34:08 +0800

    Test new database

---
+++ backend/connTest.php
@@ -0,0 +1,33 @@
+<?php
+    
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
+    
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+        
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+    echo "string:".$connectstr_dbhost.",".$connectstr_dbname;
+    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname)   ;
+    
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    
+    mysqli_close($link);
+
+?>
