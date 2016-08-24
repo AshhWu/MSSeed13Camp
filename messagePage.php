196f32f09e94832dab29a6a55257d35f5a36c369
andyliu42 <andyliu31240@gmail.com>
2016-08-24 17:28:10 +0800

    Height modify

--- messagePage.php
+++ messagePage.php
@@ -39,7 +39,7 @@ if($_SESSION['ID'] != null){
   ?>
  
     <iframe src="messagePageGM.php" width="100%" height="180"></iframe>
-    <iframe src="messagePageNormal.php" width="100%" height="300"></iframe>
+    <iframe src="messagePageNormal.php" width="100%" height="350"></iframe>
 		
 <?php
 }
