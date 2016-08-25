28540a1a44adf34460190fb4a9225c92c0a241b7
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-25 15:32:10 +0800

    remove sql

--- backend/connTest.php
+++ backend/connTest.php
@@ -19,30 +19,6 @@
         $conn = new PDO( "mysql:host=$connectstr_dbhost;dbname=$connectstr_dbname", $connectstr_dbusername, $connectstr_dbpassword);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-        // Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
-$lines = file('import.sql');
-// Loop through each line
-foreach ($lines as $line)
-{
-// Skip it if it's a comment
-if (substr($line, 0, 2) == '--' || $line == '')
-    continue;
-
-// Add this line to the current segment
-$templine .= $line;
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // Perform the query
-    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-    // Reset temp variable to empty
-    $templine = '';
-}
-}
- echo "Tables imported successfully";
-
     }
     catch(Exception $e){
         die(print_r($e));
