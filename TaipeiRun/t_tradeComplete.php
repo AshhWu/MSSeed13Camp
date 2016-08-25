b4a31c235604defd5d0e7c283d2bb62042979c22
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-25 20:43:59 +0800

    Update t_tradeComplete.php

--- TaipeiRun/t_tradeComplete.php
+++ TaipeiRun/t_tradeComplete.php
@@ -11,12 +11,11 @@ include_once '../backend/taskmodel.php';
     $fcube3 = $_GET['fc3'];
     $fcube4 = $_GET['fc4'];
     $fcube5 = $_GET['fc5'];
+    $id = $_GET['id'];
     $conn = connect();
-    $sql = "UPDATE taipeiRun SET cube2=".$cube2.", cube3=".$cube3.", cube4=".$cube4.", cube5=".$cube5." where team=".$teama;
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-
-    $sql = "UPDATE taipeiRun SET cube2=".$fcube2.", cube3=".$fcube3.", cube4=".$fcube4.", cube5=".$fcube5." where team=".$teamb;
+    $sql = "UPDATE taipeiRun SET cube2=".$cube2.", cube3=".$cube3.", cube4=".$cube4.", cube5=".$cube5." where team=".$teama."
+            ; UPDATE taipeiRun SET cube2=".$fcube2.", cube3=".$fcube3.", cube4=".$fcube4.", cube5=".$fcube5." where team=".$teamb." 
+            ; UPDATE t_tradeRequest SET state=2 WHERE id=".$id;
     $stmt = $conn->prepare($sql);
     $stmt->execute();
 
