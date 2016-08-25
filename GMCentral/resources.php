e4d7bd1794c5a5360cde29d96e890db3921bf0ea
houtoto13579 <houtoto13579@gmail.com>
2016-08-25 17:41:34 +0800

    GM message delete

--- GMCentral/resources.php
+++ GMCentral/resources.php
@@ -243,7 +243,6 @@
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.poptrox.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
