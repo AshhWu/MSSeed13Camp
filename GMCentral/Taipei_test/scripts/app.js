a209131f4a6d09cac37338588cbf2b9f99d413aa
WangChen Wu <ra960618@gmail.com>
2016-08-25 19:08:31 +0800

    test

--- GMCentral/Taipei_test/scripts/app.js
+++ GMCentral/Taipei_test/scripts/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', []);
 
-app.controller("BarCtrl", ['$scope', function ($scope) {
+app.controller('BarCtrl', ['$scope', function ($scope) {
   		$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   		$scope.series = ['Series A', 'Series B'];
 
@@ -10,10 +10,50 @@ app.controller("BarCtrl", ['$scope', function ($scope) {
   		];
 	}])
 
-	.controller('cubeController', ['$scope', 'cubeService', '$interval', function($scope, cubeService, $interval){
-
+	.controller('cubeController', ['$scope', '$interval', function($scope, $interval){
+      $scope.teamInfo = [
+            {
+                          team: "NULL"
+                      },
+            {
+                        team: "美國"
+                      },
+                      {
+                        team: "加拿大"
+                      },
+                      {
+                        team: "巴西"
+                      },
+                      {
+                        team: "義大利"
+                      },
+                      {
+                        team: "德國"
+                      },
+                      {
+                        team: "英國"
+                      },
+                      {
+                        team: "台灣"
+                      },
+                      {
+                        team: "日本"
+                      },
+                      {
+                        team: "泰國"
+                      },
+                      {
+                        team: "南非"
+                      },
+                      {
+                        team: "馬達加斯加"
+                      },
+                      {
+                        team: "埃及"
+                      }
+                  ];
 		function getCube() {
-			cubeService.getCube()
+			$http.get("scripts/CURD/getCube.php")
             .then(function(data){
                 console.log("cube get");
                 $scope.cubes = data;
@@ -24,7 +64,7 @@ app.controller("BarCtrl", ['$scope', function ($scope) {
 
         $interval(getCube, 2000);
 }])
-	.controller('locationController', ['$scope', '$interval', 'flagService', function($scope, $interval, flagService){
+	.controller('locationController', ['$scope', '$interval', function($scope, $interval){
       $scope.markers = [];
       
       var latLng = new google.maps.LatLng(25.054056, 121.539021);
@@ -35,120 +75,12 @@ app.controller("BarCtrl", ['$scope', function ($scope) {
           mapTypeId: google.maps.MapTypeId.ROADMAP
           });
       
-      $scope.teamInfo = flagService.getTeamInfo();
-      $interval(getLoc, 5000);
-
-      function getLoc(){
-        flagService.getLocation()
-        .then(function(data){
-            for(var index in data)
-            {
-              var team = data[index];
-              console.log(team);
-              console.log($scope.markers[team.Team]);
-  
-              if(team.location != 'null')
-              {
-                if($scope.markers[team.Team] === undefined)
-                {
-                    console.log('Create new marker');
-                    $scope.markers[team.Team] = new MarkerWithLabel({
-                        position: new google.maps.LatLng(team.gpsX, team.gpsY),
-                        draggable: true,
-                        raiseOnDrag: true,
-                        map: map,
-                        icon: $scope.teamInfo[team.Team].picture,
-                        labelContent: $scope.teamInfo[team.Team].team,
-                        labelAnchor: new google.maps.Point(22, 0),
-                        labelClass: "labels", // the CSS class for the label
-                        labelStyle: {opacity: 0.8}
-                    });
-                }
-                else
-                {
-                    console.log('Move');
-                    $scope.markers[team.Team].setPosition(new google.maps.LatLng(team.gpsX, team.gpsY));
-                }
-              }
-            }
-          } ,function(error)
-          { 
-              console.log("error: " + error.message);
-          });
-      }
-
-	}])
-	.controller('picController', ['$scope', 'picService', '$interval', function($scope, picService, $interval){
-     	$scope.pics = picService.getPics();
-
-     	function updatePics(){
-     		for(var i = 1; i <= $scope.pics.length; i++){
-                $scope.pics[i].picture = "img/TeamPicture/" + i + ".jpg" + '?' + new Date().getTime();
-                console.log($scope.pics[i].picture);
-            }
-     	}
-
-     	$interval(updatePics, 2000);
-
-	}])
-	.service('cubeService', ["$http", function($http){
-		var teamInfo = [
-						{
-                      	  team: "NULL"
-                    	},
-						{
-                    	  team: "美國"
-                    	},
-                    	{
-                    	  team: "加拿大"
-                    	},
-                    	{
-                    	  team: "巴西"
-                    	},
-                    	{
-                    	  team: "義大利"
-                    	},
-                    	{
-                    	  team: "德國"
-                    	},
-                    	{
-                    	  team: "英國"
-                    	},
-                    	{
-                    	  team: "台灣"
-                    	},
-                    	{
-                    	  team: "日本"
-                    	},
-                    	{
-                    	  team: "泰國"
-                    	},
-                    	{
-                    	  team: "南非"
-                    	},
-                    	{
-                    	  team: "馬達加斯加"
-                    	},
-                    	{
-                    	  team: "埃及"
-                    	}
-		              ];
-
-		this.getTeamInfo = function(){
-			return teamInfo;
-		};
-
-        this.getCube = function(){
-        	return $http.get("scripts/CURD/getCube.php");
-        };
-    }])
-    .service('flagService', ["$http", function($http){
-		var teamInfo = [
-						        {
+      $scope.teamInfo = [
+                    {
                       team: "NULL",
                       picture: "img/flag/台灣.png"
                     },
-						        {
+                    {
                       team: "美國",
                       picture: "img/flag/美國.png"
                     },
@@ -196,43 +128,52 @@ app.controller("BarCtrl", ['$scope', function ($scope) {
                       team: "埃及",
                       picture: "img/flag/埃及.png"
                     }
-		];
+    ];
+      $interval(getLoc, 5000);
 
-    this.getTeamInfo = function(){
-        return teamInfo;
-    };
+      function getLoc(){
+        $http.get("scripts/CURD/getLocation.php")
+        .then(function(data){
+            for(var index in data)
+            {
+              var team = data[index];
+              console.log(team);
+              console.log($scope.markers[team.Team]);
+  
+              if(team.location != 'null')
+              {
+                if($scope.markers[team.Team] === undefined)
+                {
+                    console.log('Create new marker');
+                    $scope.markers[team.Team] = new MarkerWithLabel({
+                        position: new google.maps.LatLng(team.gpsX, team.gpsY),
+                        draggable: true,
+                        raiseOnDrag: true,
+                        map: map,
+                        icon: $scope.teamInfo[team.Team].picture,
+                        labelContent: $scope.teamInfo[team.Team].team,
+                        labelAnchor: new google.maps.Point(22, 0),
+                        labelClass: "labels", // the CSS class for the label
+                        labelStyle: {opacity: 0.8}
+                    });
+                }
+                else
+                {
+                    console.log('Move');
+                    $scope.markers[team.Team].setPosition(new google.maps.LatLng(team.gpsX, team.gpsY));
+                }
+              }
+            }
+          } ,function(error)
+          { 
+              console.log("error: " + error.message);
+          });
+      }
 
-		this.getLocation = function(){
-			return $http.get("scripts/CURD/getLocation.php");
-			     
-		};
-
-		// this.mark = function(){
-		// 	var latLng = new google.maps.LatLng(25.054056, 121.539021);
-  //           var mapCanvas = document.getElementById('map-canvas');
-  //           var map = new google.maps.Map(mapCanvas, {
-  //               zoom: 14,
-  //               center: latLng,
-  //               mapTypeId: google.maps.MapTypeId.ROADMAP
-  //               });
-		// 	for(var index in teamInfo)
-  //      	 	{
-  //               markers[index] = new MarkerWithLabel({
-  //               position: new google.maps.LatLng(25.035346, 121.499810),
-  //               draggable: true,
-  //               raiseOnDrag: true,
-  //               map: map,
-  //               icon: teamInfo[index]['picture'],
-  //               labelContent: teamInfo[index]['team'],
-  //               labelAnchor: new google.maps.Point(25, 0),
-  //               labelClass: "labels", // the CSS class for the label
-  //               labelStyle: {opacity: 0.8}
-  //           });
-  //       };
 	}])
-	.service('picService',function(){
-		var Pics = [
-					{
+	.controller('picController', ['$scope', 'picService', '$interval', function($scope, picService, $interval){
+     	$scope.pics = Pics = [
+          {
                       team: "NULL",
                       picture: "img/TeamPicture/1.jpg"
                     },
@@ -284,8 +225,15 @@ app.controller("BarCtrl", ['$scope', function ($scope) {
                       team: "埃及",
                       picture: "img/TeamPicture/12.jpg"
                     }
-		];
-		this.getPics = function(){
-			return Pics;
-		};
-	});
+    ];
+
+     	function updatePics(){
+     		for(var i = 1; i <= $scope.pics.length; i++){
+                $scope.pics[i].picture = "img/TeamPicture/" + i + ".jpg" + '?' + new Date().getTime();
+                console.log($scope.pics[i].picture);
+            }
+     	}
+
+     	$interval(updatePics, 2000);
+
+	}]);
