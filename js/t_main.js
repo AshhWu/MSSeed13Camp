abec56adfa80966959ea312e25bcdacfb76d989a
kevinwei30 <kevinwei840223@gmail.com>
2016-08-25 21:42:09 +0800

    Add files via upload

--- js/t_main.js
+++ js/t_main.js
@@ -215,13 +215,19 @@ function trade_agree(lego, trade){
 	var team_s = trade[1];
 	var team_r = trade[2];
 
-	tmp_str = "TaipeiRun/t_tradeComplete.php?id=" + trade[0] + "&teama=" + team_r;
+	tmp_str = 'TaipeiRun/t_tradeComplete.php?id=' + trade[0] + '&teama=' + team_r;
 	for (i = 4; i <= 7; i++){
-		tmp_str += "&c" + (i-2).toString() + "=" + (lego[i-4] - trade[i]).toString();
+		tmp_str += '&c' + (i-2).toString() + '=' + (lego[i-4] - trade[i]).toString();
 	}
-	tmp_str += "&teamb=" + team_s;
+	tmp_str += '&teamb=' + team_s;
 	for (i = 9; i <= 12; i++){
-		tmp_str += "&fc" + (i-7).toString() + "=" + (lego[i-5] - trade[i]).toString();
+		tmp_str += '&fc' + (i-7).toString() + '=' + (lego[i-5] - trade[i]).toString();
 	}
 	window.location =  tmp_str;
+}
+
+function trade_reject(id){
+	alert('reject!');
+	var tmp_str = 'TaipeiRun/t_tradeFail.php?id=' + id;
+	window.location = tmp_str;
 }
