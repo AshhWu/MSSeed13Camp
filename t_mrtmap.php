1107e3d6e115c6b73ef1318246a826548e8447b6
kevinwei30 <kevinwei840223@gmail.com>
2016-08-25 12:55:57 +0800

    Update t_mrtmap.php

--- t_mrtmap.php
+++ t_mrtmap.php
@@ -21,7 +21,7 @@ session_start();
 <?php $sstation = getPosition($team); ?>
 <?php $lego = getAllCubeNum($team); ?>
 
-<h5 id="team_label" class="w3-indigo w3-padding">我是第<?php echo $team;?>組@@</h5>
+<h5 id="team_label" class="w3-indigo w3-padding">我是第<?php echo $team;?>組</h5>
 <div class="middle_part w3-container w3-section">
 	<h5 class="w3-brown w3-padding">移動</h5>
 	<section class="section1 w3-container">
