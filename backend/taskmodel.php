3d962e07abf2c414943d95c29c777843d254ce66
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-25 16:26:40 +0800

    add connection

--- backend/taskmodel.php
+++ backend/taskmodel.php
@@ -16,7 +16,7 @@
 function connect()
 {
 	// DB connection info
-	/*$host = "ap-cdbr-azure-east-c.cloudapp.net";
+	$host = "ap-cdbr-azure-east-c.cloudapp.net";
 	$user = "b37f8ddf38d21d";
 	$pwd = "1e72c81e";
 	$db = "stronghold";
@@ -28,7 +28,7 @@ function connect()
 	catch(Exception $e){
 		die(print_r($e));
 	}
-	return $conn;*/
+	return $conn;
 	/*$connectstr_dbhost = '';
     $connectstr_dbname = 'msseed13';
     $connectstr_dbusername = '';
