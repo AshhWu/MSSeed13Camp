f9097307952c75e8e036d3d858d37d7b2284cd3d
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-25 23:10:07 +0800

    hide navbar for users

--- backend/navbar/nav.php
+++ backend/navbar/nav.php
@@ -26,21 +26,13 @@ switch($_SESSION['authority']){
 			<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown">台北行 <span class="caret"></span></a>
 			<ul class="dropdown-menu">
-				<li <?=findActiveClass("t_mrtmap")?>><a href="t_mrtmap.php">移動迷宮</a></li>
-				<li <?=findActiveClass("t_missionDetail")?>><a href="t_missionDetail.php">任務資訊</a></li>
-				<li <?=findActiveClass("t_missionHint")?>><a href="t_missionHint.php">圖片提示</a></li>
-				<li <?=findActiveClass("t_lego_list")?>><a href="t_lego_list.php">樂高交易</a></li>
-				<li><a href="#">求助中心</a></li> 
+				<li><a href="#">尚未開放</a></li> 
 			</ul>
 			</li>
 			<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown">據點戰 <span class="caret"></span></a>
 			<ul class="dropdown-menu">
-				<li <?=findActiveClass("map")?>><a href="map.php">據點地圖</a></li>
-				<li <?=findActiveClass("rules")?>><a href="rules.php">遊戲規則</a></li>
-				<li <?=findActiveClass("missionPage.php")?>><a href="missionPage.php">遊戲關卡</a></li>
-				<li <?=findActiveClass("material")?>><a href="material.php">材料表</a></li>
-				<li <?=findActiveClass("messagePage")?>><a href="messagePage.php">消息公告</a></li>  
+				<li><a href="#">尚未開放</a></li> 
 			</ul>
 			</li>
 			<li><a href="/backend/login/logout.php" class="logout-btn"><span class="glyphicon glyphicon-log-out"></span> 登出</a></li>
