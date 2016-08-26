ef1b6783af9726efbced17a1afd68bb231c60fa3
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 05:49:29 +0800

    Update t_gpsTest2.php

--- TaipeiRun/t_gpsTest2.php
+++ TaipeiRun/t_gpsTest2.php
@@ -21,7 +21,7 @@
     function successCallback(position) {
       $lat=0;
       $lon=0;
-      document.location.href="t_updateGPS.php?lat=0&lon=0;
+      document.location.href="t_updateGPS.php?lat=0&lon=0";
     }
     function errorCallback(error) {
       var errorTypes={
