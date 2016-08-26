2c69d58d09eb89d053b9fd071b1f77f643b622ab
kevinwei30 <kevinwei840223@gmail.com>
2016-08-27 03:40:52 +0800

    Add files via upload

--- GMCentral/Taipei/chat.php
+++ GMCentral/Taipei/chat.php
@@ -173,9 +173,9 @@ session_start();
         str = '';
         for (i = 2; i < len; i+=5){
             if (msg[i] == 1)
-                str += '<p class="w3-card-4 w3-padding w3-light-grey" style="float:right; height:30px; max-width:300px;">' + msg[i+1] + '</p><br><br>';
+                str += '<p class="w3-card-2 w3-padding w3-light-grey" style="float:right;max-width:250px;">' + msg[i+1] + '</p><br><br>';
             else if (msg[i] == 0)
-                str += '<p class="w3-card-4 w3-padding w3-light-grey" style="float:left; height:30px; max-width:300px;">' + msg[i+1] + '</p><br><br>';
+                str += '<p class="w3-card-2 w3-padding w3-light-grey" style="float:left;max-width:250px;">' + msg[i+1] + '</p><br><br>';
         }
         document.getElementById("board" + j.toString()).innerHTML += str;
     }
