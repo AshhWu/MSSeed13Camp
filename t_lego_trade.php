ed7763fc6e4d16b9f5feb4de2d8e5913851720cd
kevinwei30 <kevinwei840223@gmail.com>
2016-08-26 19:30:57 +0800

    Update t_lego_trade.php

--- t_lego_trade.php
+++ t_lego_trade.php
@@ -23,13 +23,13 @@ session_start();
 <?php $lego = getAllCubeNum($team); ?>
 <?php $lego2 = getAllCubeNum($team2); ?>
 
-<h5 id="team_label" class="w3-indigo w3-padding"><?php echo $country;?></h5>
+<h3 id="team_label" style="color:#337ab7"><?php echo $country;?></h3>
 <div id="team2" style="display:none"><?php echo $team2;?></div>
-<div id="trade_with" class="w3-container w3-section w3-card w3-padding w3-brown"></div>
-<div id="warning" class="w3-container w3-section w3-red">
+<div id="trade_with" class="w3-container w3-section w3-card-2 w3-padding"></div>
+<div id="warning" class="w3-container w3-section w3-red w3-card-4">
 	<span onclick="this.parentElement.style.display='none'" class="w3-closebtn">&times;</span>
-  	<h3>注意!</h3>
-  	<p>1.雙方交易用的樂高數量請不要超出合理範圍(ex. 我有10個紅色樂高,輸入框請填0~10範圍內的數字)</p><br>
+  	<h3>注意!</h3><br>
+  	<p>1.雙方交易用的樂高數量請不要超出合理範圍(ex.我有10個紅色樂高,輸入框請填0~10範圍內的數字)</p><br>
   	<p>2.請不要在輸入框留空白.每個輸入框預設都為0,請勿把0刪除後還不填數字.</p><br>
   	<p>3.如果違反以上兩點,樂高可能不翼而飛或交易失敗,請自行負責.<p>
 </div>
@@ -41,7 +41,7 @@ session_start();
 	document.getElementById("trade_with").innerHTML += '<br>聯絡電話: ' + phone[document.getElementById("team2").innerText];
 </script>
 
-<table id="trade_table" class="w3-table w3-sand">
+<table id="trade_table" class="w3-table w3-card-2">
 <tr>
 	<th class="w3-tag">我方</th>
 	<th>給對方</th>
