5561814860b0a8f9ce5f19888b49647070dd2df2
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 00:03:28 +0800

    Update t_addHelpMsg.php

--- TaipeiRun/t_addHelpMsg.php
+++ TaipeiRun/t_addHelpMsg.php
@@ -6,7 +6,7 @@ $team = $_GET['team'];
 $content = $_GET['content'];
 
 $conn = connect();
-$sql = "INSERT INTO t_helpCenter (team, isGM, content, state) VALUES (?, 0, ?, 0)";
+$sql = "INSERT INTO t_helpCenter (sender, isGM, content, state) VALUES (?, 0, ?, 0)";
 $stmt = $conn->prepare($sql);
 $stmt->bindValue(1, $team);
 $stmt->bindValue(2, $content);
