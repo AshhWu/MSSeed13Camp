496278f2ebe67f94522e67675f16732ec074653e
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-26 14:42:00 +0800

    Update t_taipeiAdmin.php

--- t_taipeiAdmin.php
+++ t_taipeiAdmin.php
@@ -135,20 +135,21 @@ session_start();
             {
                 echo 	"<tr>
                             <td>".$item[0]."</td>
-                            <td>".$item[1]."</td>";
+                            <td>".$item[1]."</td>
+                            <td>".$item[2]."</td>";
                /* if(!empty($item[3])){
                     echo    "<td><img height='200px' src='data:image/jpeg;base64,".base64_encode($item[3])."' /></td>";
                 }else{*/
                     echo    "<td>無圖片</td>";
                 //}
-                if($item[4]==0){
+                /*if($item[4]==0){
                     echo    "<td><a href='t_missionComplete.php?id=".$item[0]."&team=".$item[1]."'>OK</a> or 
                                  <a href='t_missionFail.php?id=".$item[0]."&team=".$item[1]."'>NOT</a></td>";
                 }else if($item[4]==1){
                     echo    "<td>Complete</td>";
                 }else{
                     echo    "<td>Fail</td>";
-                }
+                }*/
                 echo    "</tr>";
             }
 
