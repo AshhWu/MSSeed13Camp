2050c05e4faccd23e0736ac62f8fb9e8e18af669
WangChen Wu <ra960618@gmail.com>
2016-08-26 18:57:39 +0800

    final

--- GMCentral/Taipei_test/scripts/app.js
+++ GMCentral/Taipei_test/scripts/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('app', ["chart.js"]);
 
 app.controller('cubeController', ['$scope','$http', '$interval', function($scope, $http, $interval){
-      $scope.labels = ['美國', '加拿大', '巴西', '義大利', '德國', '英國', '台灣', '日本', '泰國', '南非', '馬達加斯加', '埃及'];
-      $scope.totalCube = [];
+    $scope.labels = ['美國', '加拿大', '巴西', '義大利', '德國', '英國', '台灣', '日本', '泰國', '南非', '馬達加斯加', '埃及'];
+    $scope.totalCube = [];
 
     $scope.colorCube = [
                       {
@@ -99,115 +99,9 @@ app.controller('cubeController', ['$scope','$http', '$interval', function($scope
              });
         }
         getCube();
-        $interval(getCube, 2000);
+        $interval(getCube, 5000);
 }])
-// 	.controller('locationController', ['$scope','$http', '$interval', function($scope, $http, $interval){
-//       $scope.markers = [];
-      
-//       var latLng = new google.maps.LatLng(25.054056, 121.539021);
-//       var mapCanvas = document.getElementById('map-canvas');
-//       var map = new google.maps.Map(mapCanvas, {
-//           zoom: 14,
-//           center: latLng,
-//           mapTypeId: google.maps.MapTypeId.ROADMAP
-//           });
-      
-//       $scope.teamInfo = [
-//                     {
-//                       team: "NULL",
-//                       picture: "img/flag/台灣.png"
-//                     },
-//                     {
-//                       team: "美國",
-//                       picture: "img/flag/美國.png"
-//                     },
-//                     {
-//                       team: "加拿大",
-//                       picture: "img/flag/加拿大.png"
-//                     },
-//                     {
-//                       team: "巴西",
-//                       picture: "img/flag/巴西.png"
-//                     },
-//                     {
-//                       team: "義大利",
-//                       picture: "img/flag/義大利.png"
-//                     },
-//                     {
-//                       team: "德國",
-//                       picture: "img/flag/德國.png"
-//                     },
-//                     {
-//                       team: "英國",
-//                       picture: "img/flag/英國.png"
-//                     },
-//                     {
-//                       team: "台灣",
-//                       picture: "img/flag/台灣.png"
-//                     },
-//                     {
-//                       team: "日本",
-//                       picture: "img/flag/日本.png"
-//                     },
-//                     {
-//                       team: "泰國",
-//                       picture: "img/flag/泰國.png"
-//                     },
-//                     {
-//                       team: "南非",
-//                       picture: "img/flag/南非.png"
-//                     },
-//                     {
-//                       team: "馬達加斯加",
-//                       picture: "img/flag/馬達加斯加.png"  
-//                     },
-//                     {
-//                       team: "埃及",
-//                       picture: "img/flag/埃及.png"
-//                     }
-//     ];
-//       $interval(getLoc, 5000);
 
-//       function getLoc(){
-//         $http.get("scripts/CURD/getLocation.php")
-//         .then(function(data){
-//             for(var index in data)
-//             {
-//               var team = data[index];
-//               console.log(team);
-//               console.log($scope.markers[team.Team]);
-  
-//               if(team.location != 'null')
-//               {
-//                 if($scope.markers[team.Team] === undefined)
-//                 {
-//                     console.log('Create new marker');
-//                     $scope.markers[team.Team] = new MarkerWithLabel({
-//                         position: new google.maps.LatLng(team.gpsX, team.gpsY),
-//                         draggable: true,
-//                         raiseOnDrag: true,
-//                         map: map,
-//                         icon: $scope.teamInfo[team.Team].picture,
-//                         labelContent: $scope.teamInfo[team.Team].team,
-//                         labelAnchor: new google.maps.Point(22, 0),
-//                         labelClass: "labels", // the CSS class for the label
-//                         labelStyle: {opacity: 0.8}
-//                     });
-//                 }
-//                 else
-//                 {
-//                     console.log('Move');
-//                     $scope.markers[team.Team].setPosition(new google.maps.LatLng(team.gpsX, team.gpsY));
-//                 }
-//               }
-//             }
-//           } ,function(error)
-//           { 
-//               console.log("error: " + error.message);
-//           });
-//       }
-
-// 	}])
 	.controller('picController', ['$scope', '$interval', '$http', function($scope, $interval, $http){
      	$scope.pics =  [
                     {
@@ -267,7 +161,7 @@ app.controller('cubeController', ['$scope','$http', '$interval', function($scope
             }
      	}
 
-     	$interval(updatePics, 5000);
+     	$interval(updatePics, 10000);
 
 	}])
 
