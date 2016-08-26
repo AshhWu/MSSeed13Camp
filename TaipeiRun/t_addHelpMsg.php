31065c57376a5493d0c2c11aa2b797e5749945ab
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-26 23:56:50 +0800

    Update t_addHelpMsg.php

--- TaipeiRun/t_addHelpMsg.php
+++ TaipeiRun/t_addHelpMsg.php
@@ -3,15 +3,13 @@
 include_once '../backend/taskmodel.php';
 
 $team = $_GET['team'];
-$isGM = $_GET['isGM'];
 $content = $_GET['content'];
 
 $conn = connect();
-$sql = "INSERT INTO t_helpCenter (team, isGM, content, state) VALUES (?, ?, ?, 0)";
+$sql = "INSERT INTO t_helpCenter (team, isGM, content, state) VALUES (?, 0, ?, 0)";
 $stmt = $conn->prepare($sql);
 $stmt->bindValue(1, $team);
-$stmt->bindValue(2, $isGM);
-$stmt->bindValue(3, $content);
+$stmt->bindValue(2, $content);
 $stmt->execute();
 
 header('Location: ../t_helpCenter.php');
