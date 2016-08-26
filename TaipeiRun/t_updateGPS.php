b3c822b88b7a9847cd4a2bf1793a1691c8cc6e5c
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 07:51:54 +0800

    Update t_updateGPS.php

--- TaipeiRun/t_updateGPS.php
+++ TaipeiRun/t_updateGPS.php
@@ -4,6 +4,9 @@
     $lat = $_GET['lat'];
     $lon = $_GET['lon'];
     
+    str_replace ("_",".",$lat);
+    str_replace ("_",".",$lon);
+    
     $conn = connect();
     $sql = "UPDATE taipeiRun SET gpsX=".$lat.", gpsY=".$lon." where team=2";
     $stmt = $conn->prepare($sql);
