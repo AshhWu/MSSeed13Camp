2572883e708bfb9aa0015a1f18895da9d5c770ae
kevinwei30 <kevinwei840223@gmail.com>
2016-08-26 22:32:37 +0800

    Add files via upload

--- t_helpCenter.php
+++ t_helpCenter.php
@@ -24,8 +24,8 @@ session_start();
 <h3 style="color:#337ab7; width:180px; margin:auto; text-align: center">求助中心</h3>
 
 <div id="chat_board" class="w3-container w3-section w3-card-2 w3-padding" style="width:320px; height:350px; margin:auto;">
-	<p class="w3-card-4 w3-padding" style="float:left">hi!</p>
-	<p class="w3-card-4 w3-padding w3-grey" style="float:right">hello~</p>
+	<p class="w3-card-4 w3-padding" style="float:left">hi!</p><br>
+	<p class="w3-card-4 w3-padding w3-light-grey" style="float:right">hello~</p><br>
 </div>
 
 <div class="w3-container w3-section w3-card-2 w3-padding" style="width: 320px; margin: auto;">
