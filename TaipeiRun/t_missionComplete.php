8e9fb5352a2ac8834659cd8e12bb9f1c092105eb
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-26 12:23:48 +0800

    Create t_missionComplete.php

---
+++ TaipeiRun/t_missionComplete.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../backend/taskmodel.php';
+$id = $_GET['id'];
+$team = $_GET['team'];
+$conn = connect();
+$sql = "UPDATE t_missionReport SET state=1 where id=".$id;
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$sql = "UPDATE taipeiRun SET mState=2 where team=?";
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(1, $team);
+$stmt->execute();
+header('Location: t_taipeiAdmin.php');
+?>
