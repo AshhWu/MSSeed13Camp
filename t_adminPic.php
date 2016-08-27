6d746c6470e952c0daf32a2b5d58028da25a238b
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 14:21:53 +0800

    Update t_adminPic.php

--- t_adminPic.php
+++ t_adminPic.php
@@ -58,7 +58,7 @@ session_start();
                             <td>".$item[0]."</td>
                             <td>".$item[1]."</td>
                             <td>".$item[2]."</td>";
-                if(!empty($item[3])&&$item[0]>281){
+                if(!empty($item[3])&&$item[0]>288){
                     echo    "<td><img height='200px' src='data:image/jpeg;base64,".base64_encode($item[3])."' /></td>";
                 }else{
                     echo    "<td>無圖片</td>";
