9bb9cdad64ae3473765416a4b72e6a8a118aff56
andyliu42 <andyliu31240@gmail.com>
2016-08-27 11:08:47 +0800

    connect

--- auto_add.php
+++ auto_add.php
@@ -14,7 +14,6 @@
 	*/
 	
 echo "Hello world";
-/*
 function connect()
 {
 	$connectstr_dbhost = '127.0.0.1:50919';
@@ -45,6 +44,7 @@ function connect()
     return $conn;
 }
 
+/*
 
 function sqlcode($sql)
 {
