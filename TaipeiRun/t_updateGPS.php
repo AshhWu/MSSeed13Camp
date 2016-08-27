8f4c8572e069e598d7488cfbd55ecfc371ec38b7
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 10:07:31 +0800

    Update t_updateGPS.php

--- TaipeiRun/t_updateGPS.php
+++ TaipeiRun/t_updateGPS.php
@@ -8,7 +8,7 @@
     //str_replace ("[_]",".",$lon);
 
     $lat_sec = explode("_",$lat);
-    $lat = $lat_sec[0] + "." + $lat_sec[1];
+    $lat = $lat_sec[0].".".$lat_sec[1];
     
     //$lat=1.1;
     $lon=3.1;
