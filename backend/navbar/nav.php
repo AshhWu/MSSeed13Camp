276b5bbdd11c632e3db0425519fde5e67928ca87
mnbvy <mnbvyzxc@yahoo.com.tw>
2016-08-27 12:01:59 +0800

    Update nav.php

--- backend/navbar/nav.php
+++ backend/navbar/nav.php
@@ -26,7 +26,11 @@ switch($_SESSION['authority']){
 			<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown">台北行 <span class="caret"></span></a>
 			<ul class="dropdown-menu">
-				<li><a href="#">尚未開放</a></li> 
+				<li <?=findActiveClass("t_mrtmap")?>><a href="t_mrtmap.php">移動迷宮</a></li>
+				<li <?=findActiveClass("t_missionDetail")?>><a href="t_missionDetail.php">任務資訊</a></li>
+				<li <?=findActiveClass("t_missionHint")?>><a href="t_missionHint.php">圖片提示</a></li>
+				<li <?=findActiveClass("t_lego_list")?>><a href="t_lego_list.php">樂高交易</a></li>
+				<li <?=findActiveClass("t_helpCenter")?>><a href="t_helpCenter.php">求助中心</a></li>
 			</ul>
 			</li>
 			<li class="dropdown">
